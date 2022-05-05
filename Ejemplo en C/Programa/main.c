91a8b8f76344a4dfd90aebde0b8ef25646ad96b2
sebas <sebas@gmail.com>
2022-05-05 11:57:41 -0500

    6

--- Ejemplo en C/Programa/main.c
+++ Ejemplo en C/Programa/main.c
@@ -1,5 +1,4 @@
 #include <stdlib.h>
-#include <stdio.h>
 #include <string.h>
 #include <stdio.h>
 #include <unistd.h>
@@ -14,30 +13,33 @@
 #include "Player.h"
 #include "Machete.h"
 
-static VideoJuego *nuevo_video_juego;
-static Enemigo *l_enemigos[10];
-static bool tecla_presionada = false;
-static char buffer_tecla;
-static int mira = 0;
-static int tno_l_enemigos = -1;
+struct threadParam_t
+{
+    VideoJuego *nuevo_video_juego;
+    Enemigo (*l_enemigos)[10];
+    bool tecla_presionada;
+    char * buffer_tecla;
+    int mira;
+    int tno_l_enemigos;
+};
 
 void *Juego(void *data)
 {
-
+    struct threadParam_t *dataTmp = (struct threadParam_t*)data;
     while (true)
     {
         printf("...");
-        if (Ganador(nuevo_video_juego) == true)
+        if (Ganador(dataTmp->nuevo_video_juego) == true)
         {
 
-            printf("Ganaste el juego... Puntos: %i", nuevo_video_juego->puntos);
+            printf("Ganaste el juego... Puntos: %i", dataTmp->nuevo_video_juego->puntos);
             sleep(5);
             return EXIT_SUCCESS;
         }
-        else if (Perdedor(nuevo_video_juego) == true)
+        else if (Perdedor(dataTmp->nuevo_video_juego) == true)
         {
 
-            printf("Perdiste el juego... Puntos: %i", nuevo_video_juego->puntos);
+            printf("Perdiste el juego... Puntos: %i", dataTmp->nuevo_video_juego->puntos);
             sleep(5);
             return EXIT_SUCCESS;
         }
@@ -48,41 +50,44 @@ void *Juego(void *data)
 
 void *Teclado_P(void *data)
 {
+    struct threadParam_t *dataTmp = (struct threadParam_t*)data;
+    
     while (true)
     {
-        scanf("%c", &buffer_tecla);
-        tecla_presionada = true;
+        scanf("%c", dataTmp->buffer_tecla);
+        dataTmp->tecla_presionada = true;
 
-        if (tecla_presionada)
+        if (dataTmp->tecla_presionada)
         {
-            tecla_presionada = false;
-            switch (buffer_tecla)
+            dataTmp->tecla_presionada = false;
+            switch ( *(dataTmp->buffer_tecla))
             {
             case 'a':
-                Recargar_Arma(nuevo_video_juego->soldado);//
+                Recargar_Arma(dataTmp->nuevo_video_juego->soldado); //
                 break;
             case 's':
-                Disparar_Arma(nuevo_video_juego->soldado, l_enemigos[mira], nuevo_video_juego);
-                printf("Mira apuntando a Zombie Nro: %i", mira);
+                Disparar_Arma(dataTmp->nuevo_video_juego->soldado, dataTmp->l_enemigos[dataTmp->mira], dataTmp->nuevo_video_juego);
+                printf("Mira apuntando a Zombie Nro: %i", dataTmp->mira);
                 break;
             case 'd':
-                Cambiar_Arma(nuevo_video_juego->soldado);
+                Cambiar_Arma(dataTmp->nuevo_video_juego->soldado);
                 break;
             case 'w':
-                mira++;
-                if (mira == tno_l_enemigos)
+                dataTmp->mira++;
+                if (dataTmp->mira == dataTmp->tno_l_enemigos)
                 {
-                    mira = -1;
+                    dataTmp->mira = -1;
                 }
-                printf("Mira cambiada a Zombie Nro: %i", mira);
+                printf("Mira cambiada a Zombie Nro: %i", dataTmp->mira);
                 break;
             case 'x':
-                Lanzar_Machete(l_enemigos[mira], nuevo_video_juego->soldado);
+                Lanzar_Machete(dataTmp->l_enemigos[dataTmp->mira], dataTmp->nuevo_video_juego->soldado);
                 break;
             case 'c':
-                tno_l_enemigos++;
-                l_enemigos[tno_l_enemigos] = Crear_Enemigo(50);
-                printf("Hay %i zombies con vida.", tno_l_enemigos);
+                dataTmp->tno_l_enemigos++;
+                //+ dataTmp->tno_l_enemigos))    
+                dataTmp->l_enemigos[0]  = Crear_Enemigo(50);
+                printf("Hay %i zombies con vida.", dataTmp->tno_l_enemigos);
                 break;
             default:
                 break;
@@ -94,24 +99,23 @@ void *Teclado_P(void *data)
 int main(int argc, char *argv[])
 {
     // Crear el video juego en cosola
-    nuevo_video_juego = Crear_Video_Juego();
+    VideoJuego *nuevo_video_juego = Crear_Video_Juego();
+    Enemigo l_enemigos[10];
+    char buffer;
     for (int8_t i = 0; i < 10; i++)
     {
-        l_enemigos[i] = (Enemigo *)malloc(sizeof(Enemigo));
-    }
-    for (int8_t i = 0; i < 3; i++)
-    {
-        Enemigo *nuevo_enemigo = Crear_Enemigo(50);
-        l_enemigos[i] = nuevo_enemigo;
-        tno_l_enemigos++;
+        l_enemigos[i] = *(Enemigo *)malloc(sizeof(Enemigo));
     }
 
     // Treads teclado y game
     pthread_t thread1;
     pthread_t thread2;
+    
 
-    pthread_create(&thread1, NULL, &Teclado_P, 0);
-    pthread_create(&thread2, NULL, &Juego, 0);
+    struct threadParam_t threadParam = {nuevo_video_juego, (Enemigo (*)[])l_enemigos, false, &buffer ,0, 0};
+
+    pthread_create(&thread1, NULL, &Teclado_P, &threadParam);
+    pthread_create(&thread2, NULL, &Juego, &threadParam);
 
     pthread_join(thread1, NULL);
     pthread_join(thread2, NULL);
